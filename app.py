96a575582eb82507ac770753ff97d175fb029328
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-25 20:23:11 -0300

    mysql works

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, Response, Request
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 import json
+import os
+
+import dotenv
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+secret = os.getenv("mysql_password")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/testando'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{secret}@localhost/test'
 
 db = SQLAlchemy(app)
 
